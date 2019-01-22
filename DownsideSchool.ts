c7eaff4a0d2e8f05d910fe9c03a4493c3b258bb3
DevScienceScope <developer@sciencescope.uk>
2019-01-22 16:01:45 +0000

    Create DownsideSchool.ts

---
+++ DownsideSchool.ts
@@ -0,0 +1,17 @@
+
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#D31245 icon="" block="Downside School"
+namespace downside {
+    /**
+     * TODO: describe your function here
+     * @param n is value of ASCII, eg: 90
+     */
+    //% block="getASCII %v"
+    export function getASCII(n: number): string {
+        // Add code here
+        return String.fromCharCode(n);
+    }
+}
